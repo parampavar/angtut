907239a037fdd29202f0bfff324663b65f89112e
Param Pavar <github@parampavar.com>
2013-11-14 21:36:40 -0800

    ProcessFiles

---
+++ ProcessFiles/ProcessFiles/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace ProcessFiles
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Form1()
+        {
+            InitializeComponent();
+            log.Info("Info logging");
+        }
+    }
+}
