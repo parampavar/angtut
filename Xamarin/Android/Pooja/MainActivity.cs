5cd812dc5f1925cb97557dcd40983f70a055de30
Param Pavar <github@parampavar.com>
2013-12-05 17:20:41 -0800

    Added Xamarin Pooja Project

---
+++ Xamarin/Android/Pooja/MainActivity.cs
@@ -0,0 +1,37 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Pooja
+{
+	[Activity (Label = "Pooja", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			button.Click += sayHello;
+			
+		}
+		public void sayHello(object sender, EventArgs e)
+		{
+			TextView lblMessage = FindViewById<TextView> (Resource.Id.lblMessage);
+			lblMessage.Text = string.Format ("{0} clicks!", count++);
+		}
+	}
+}
+
+
