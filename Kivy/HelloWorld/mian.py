f6161052328a8ff59d8e3b88eb2ad0f3f00e2cd7
Param Pavar <github@parampavar.com>
2013-12-16 15:09:07 -0800

    Kivy Pong Page

--- Kivy/HelloWorld/mian.py
+++ Kivy/HelloWorld/mian.py
@@ -2,14 +2,53 @@ import kivy
 kivy.require('1.7.2') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
+class PongBall(Widget):
 
-class MyApp(App):
+    # velocity of the ball on x and y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    # referencelist property so we can use ball.velocity as
+    # a shorthand, just like e.g. w.pos for w.x and w.y
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # ``move`` function will move the ball one step. This
+    #  will be called in equal intervals to animate the ball
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+		
+class PongGame(Widget):
+	ball = ObjectProperty(None)
+	
+	def serve_ball(self):
+		self.ball.center = self.center
+		self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+	
+	def update(self, dt):
+		self.ball.move()
+		
+		if (self.ball.y < 0) or (self.ball.top > self.height):
+			self.ball.velocity_y *= -1
+		if (self.ball.x < 0) or (self.ball.right > self.width):
+			self.ball.velocity_x *= -1
+
+class PongApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+		game = PongGame()
+		game.serve_ball()
+		Clock.schedule_interval(game.update, 1.0/60.0)
+		return game
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    PongApp().run()
