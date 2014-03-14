0ec3db7dcf4ade486b4266a3c8b5bb41ca8e0c71
Param Pavar <github@parampavar.com>
2014-03-14 08:20:37 -0700

    PEP Formatted

--- flask/FeedConfigManager/FeedProcessor/Celery/Test/processfiles.py
+++ flask/FeedConfigManager/FeedProcessor/Celery/Test/processfiles.py
@@ -35,178 +35,197 @@ masterTenantid = 0
 DocumentType = "FEEDCONFIG"
 
 
-
 class DatabaseTask(Task):
-	abstract = True
-	_db = None
-	_MasterFeedDefinitions = {}
+    abstract = True
+    _db = None
+    _MasterFeedDefinitions = {}
 
-	@property
-	def cb(self):
-		if self._db is None:
-			self._db = Couchbase.connect(bucket=DATABASENAME)
-		return self._db
+    @property
+    def cb(self):
+        if self._db is None:
+            self._db = Couchbase.connect(bucket=DATABASENAME)
+        return self._db
 
-	@property
-	def MasterFeedDefinitions(self):
-		if self._db is None:
-			self._db = Couchbase.connect(bucket=DATABASENAME)
-			_MasterFeedDefinitions = self._db.get(DOCUMENTTYPEKEYFORMAT.format(masterTenantid, documentType)).value
-		return _MasterFeedDefinitions
+    @property
+    def MasterFeedDefinitions(self):
+        if self._db is None:
+            self._db = Couchbase.connect(bucket=DATABASENAME)
+            _MasterFeedDefinitions = self._db.get(
+                DOCUMENTTYPEKEYFORMAT.format(masterTenantid, documentType)).value
+        return _MasterFeedDefinitions
+
 
 @app.task(base=DatabaseTask)
 def startProcess(tenantid, documentType):
-	path = "feeds/"
-	DocumentType = documentType
-	cbDocument = startProcess.cb.get(DOCUMENTTYPEKEYFORMAT.format(tenantid, documentType)).value
-	if ( cbDocument ):
-		
-		for infile in glob.glob( os.path.join(path, '*.txt') ):
-			logger.info("Processing file : %s" % infile)
-			logger.debug("Acquiring lock for file : %s" % infile)
-			filelock = locking.acquire_lockr(infile, block=1)
-			logger.debug("Acquired lock for file : %s" % infile)
-			locking.release_lock(filelock)
-			logger.debug("Releasing lock for file : %s" % infile)
-			
-			with open(infile, 'r') as content_file:
-				content = content_file.read()
+    path = "feeds/"
+    DocumentType = documentType
+    cbDocument = startProcess.cb.get(
+        DOCUMENTTYPEKEYFORMAT.format(tenantid, documentType)).value
+    if (cbDocument):
 
-			lines = content.splitlines()
-			firstline = lines[0]
-			lastline = lines[len(lines)-1]
-			
-			trlRowCount = int(lastline.split("|")[1])
-			if ( trlRowCount == (len(lines) - 2) ):
-				if (trlRowCount > 0):
-					for k, v in cbDocument["CONFIGS"].items():
-						if infile.find(k) > 0:
-							feedtype = k
-							rowkeyschema = v['rowkeyschema']
-							rowschema = v['rowschema']
-							
-							for lineno, line in enumerate(lines):
-								if line.startswith('HDR'):
-									pass
-								elif line.startswith('TRL'):
-									pass
-								else:
-									try:
-										insertLine(infile, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
-										logger.debug (line)
-									except (RowSchemaLessMismatchException, RowSchemaMoreMismatchException) as eSchemaEx:
-										logger.debug (eSchemaEx)
-									except (RowDuplicateException) as eEx:
-										logger.debug (eEx)
-									except (Exception) as ex:
-										logger.debug (ex)
-									# linelist = cb.get(linevalues['dictline']).value
-									# linelist
-									#deleteLine(rowkeyschema, rowschema, 1, feedtype, infile, line)
-									#insertLine(rowkeyschema, rowschema, 1, feedtype, infile, line)
-									pass
-					logger.info ("Successfully processed file : " + infile + " with " + str(trlRowCount) + " lines.")
-				elif (trlRowCount == 0):
-					logger.info ("Successfully processed file : " + infile + " with " + str(trlRowCount) + " line.")
-			else:
-				logger.info("Rowcount mismatch")
-	
+        for infile in glob.glob(os.path.join(path, '*.txt')):
+            logger.info("Processing file : %s" % infile)
+            logger.debug("Acquiring lock for file : %s" % infile)
+            filelock = locking.acquire_lockr(infile, block=1)
+            logger.debug("Acquired lock for file : %s" % infile)
+            locking.release_lock(filelock)
+            logger.debug("Releasing lock for file : %s" % infile)
+
+            with open(infile, 'r') as content_file:
+                content = content_file.read()
+
+            lines = content.splitlines()
+            firstline = lines[0]
+            lastline = lines[len(lines) - 1]
+
+            trlRowCount = int(lastline.split("|")[1])
+            if (trlRowCount == (len(lines) - 2)):
+                if (trlRowCount > 0):
+                    for k, v in cbDocument["CONFIGS"].items():
+                        if infile.find(k) > 0:
+                            feedtype = k
+                            rowkeyschema = v['rowkeyschema']
+                            rowschema = v['rowschema']
+
+                            for lineno, line in enumerate(lines):
+                                if line.startswith('HDR'):
+                                    pass
+                                elif line.startswith('TRL'):
+                                    pass
+                                else:
+                                    try:
+                                        insertLine(
+                                            infile, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
+                                        logger.debug(line)
+                                    except (RowSchemaLessMismatchException, RowSchemaMoreMismatchException) as eSchemaEx:
+                                        logger.debug(eSchemaEx)
+                                    except (RowDuplicateException) as eEx:
+                                        logger.debug(eEx)
+                                    except (Exception) as ex:
+                                        logger.debug(ex)
+                                    # linelist = cb.get(linevalues['dictline']).value
+                                    # linelist
+                                    #deleteLine(rowkeyschema, rowschema, 1, feedtype, infile, line)
+                                    #insertLine(rowkeyschema, rowschema, 1, feedtype, infile, line)
+                                    pass
+                    logger.info("Successfully processed file : " +
+                                infile + " with " + str(trlRowCount) + " lines.")
+                elif (trlRowCount == 0):
+                    logger.info("Successfully processed file : " +
+                                infile + " with " + str(trlRowCount) + " line.")
+            else:
+                logger.info("Rowcount mismatch")
+
+
 def lineToDictionary(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line):
-	dictline = {}
-	dictline['updatedatetime'] = arrow.utcnow().isoformat()
-	dictline['insertdatetime'] = arrow.utcnow().isoformat()
-	dictline['changetype'] = 2
-	dictline['isinfeed'] = 1
-	dictline['deleteflag'] = 0
-	dictline['rejectedflag'] = 0
-	dictline['errorflag'] = 0
-	dictline['tenantid'] = tenantid
-	dictline['feedtype'] = feedtype
-	dictline['filename'] = filename
-	dictline['line'] = line
+    dictline = {}
+    dictline['updatedatetime'] = arrow.utcnow().isoformat()
+    dictline['insertdatetime'] = arrow.utcnow().isoformat()
+    dictline['changetype'] = 2
+    dictline['isinfeed'] = 1
+    dictline['deleteflag'] = 0
+    dictline['rejectedflag'] = 0
+    dictline['errorflag'] = 0
+    dictline['tenantid'] = tenantid
+    dictline['feedtype'] = feedtype
+    dictline['filename'] = filename
+    dictline['line'] = line
 
-	tokens = line.split('|')
-	
-	if ( len(rowschema) == len(tokens) ):
-		for i, token in enumerate(tokens):
-			if token:
-				dictline[rowschema[i]] = token
-	
-		key= str(tenantid) + "|" + str(feedtype)
-		keylayout= "tenantid|feedtype"
-		
-		for j, keyname in enumerate(rowkeyschema):
-			key = key + "|" + tokens[rowschema.index(keyname)]
-			keylayout = keylayout + "|" + keyname 
+    tokens = line.split('|')
 
-		if dictline.__contains__('Name'):
-			dictline['NameLCase'] = dictline['Name'].lower()
-		if dictline.__contains__('FirstName'):
-			dictline['FirstNameLCase'] = dictline['FirstName'].lower()
-		if dictline.__contains__('LastName'):
-			dictline['LastNameLCase'] = dictline['LastName'].lower()
-		if dictline.__contains__('MiddleName'):
-			dictline['MiddleNameLCase'] = dictline['MiddleName'].lower()
+    if (len(rowschema) == len(tokens)):
+        for i, token in enumerate(tokens):
+            if token:
+                dictline[rowschema[i]] = token
 
-		linevalues = {}
-		linevalues['linekey'] = key
-		linevalues['linekeylayout'] = keylayout
-		linevalues['dictline'] = dictline
+        key = str(tenantid) + "|" + str(feedtype)
+        keylayout = "tenantid|feedtype"
 
-		return None, linevalues
-	elif ( len(rowschema) != len(tokens) ):
-		dictline['rejectedflag'] = 1
-		dictline['errorflag'] = 1
-		linevalues = {}
-		exceptionLinekey = "{0}|{1}|{2}|{3}|{4}|{5}".format(tenantid, documentType, "EXCEPTION", feedtype, filename, shortuuid.uuid())
-		linevalues['linekey'] = exceptionLinekey
-		linevalues['linekeylayout'] = "{0}|{1}|{2}|{3}|{4}|{5}".format("tenantid", "documentType", "EXCEPTION", "feedtype", "filename", "shortuuid")
-		linevalues['dictline'] = dictline
+        for j, keyname in enumerate(rowkeyschema):
+            key = key + "|" + tokens[rowschema.index(keyname)]
+            keylayout = keylayout + "|" + keyname
 
-		if ( len(rowschema) > len(tokens) ):
-			logger.debug(exceptionLinekey)
-			return RowSchemaLessMismatchException(filename, lineno, line), linevalues
-		elif ( len(rowschema) < len(tokens) ):
-			logger.debug(exceptionLinekey)
-			return RowSchemaMoreMismatchException(filename, lineno, line), linevalues
-		
+        if dictline.__contains__('Name'):
+            dictline['NameLCase'] = dictline['Name'].lower()
+        if dictline.__contains__('FirstName'):
+            dictline['FirstNameLCase'] = dictline['FirstName'].lower()
+        if dictline.__contains__('LastName'):
+            dictline['LastNameLCase'] = dictline['LastName'].lower()
+        if dictline.__contains__('MiddleName'):
+            dictline['MiddleNameLCase'] = dictline['MiddleName'].lower()
+
+        linevalues = {}
+        linevalues['linekey'] = key
+        linevalues['linekeylayout'] = keylayout
+        linevalues['dictline'] = dictline
+
+        return None, linevalues
+    elif (len(rowschema) != len(tokens)):
+        dictline['rejectedflag'] = 1
+        dictline['errorflag'] = 1
+        linevalues = {}
+        exceptionLinekey = "{0}|{1}|{2}|{3}|{4}|{5}".format(
+            tenantid, documentType, "EXCEPTION", feedtype, filename, shortuuid.uuid())
+        linevalues['linekey'] = exceptionLinekey
+        linevalues['linekeylayout'] = "{0}|{1}|{2}|{3}|{4}|{5}".format(
+            "tenantid", "documentType", "EXCEPTION", "feedtype", "filename", "shortuuid")
+        linevalues['dictline'] = dictline
+
+        if (len(rowschema) > len(tokens)):
+            logger.debug(exceptionLinekey)
+            return RowSchemaLessMismatchException(filename, lineno, line), linevalues
+        elif (len(rowschema) < len(tokens)):
+            logger.debug(exceptionLinekey)
+            return RowSchemaMoreMismatchException(filename, lineno, line), linevalues
+
+
 def insertLine(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line):
-	anyException, linevalues = lineToDictionary(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
-	dictline =  linevalues['dictline']
+    anyException, linevalues = lineToDictionary(
+        filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
+    dictline = linevalues['dictline']
 
-	dictline['keylayout'] = linevalues['linekeylayout']
-	logger.info ("insertLine:" + linevalues['linekey'])
+    dictline['keylayout'] = linevalues['linekeylayout']
+    logger.info("insertLine:" + linevalues['linekey'])
 
-	try:
-		startProcess.cb.add(linevalues['linekey'], dictline)
-		if anyException: 
-			raise anyException
-	except CouchbaseError as e:
-		if (e is KeyExistsError):
-			logger.debug ('insertLine: Exception={3}, FileName={0}, LineNo={1}, Line={2}'.format(filename, lineno, line, "RowDuplicateException"))
-			raise RowDuplicateException(filename, lineno, line)
-		else:
-			logger.debug ('insertLine: Exception={3}, FileName={0}, LineNo={1}, Line={2}'.format(filename, lineno, line, e.__class__))
-			raise RowException(filename, lineno, line)
-	
+    try:
+        startProcess.cb.add(linevalues['linekey'], dictline)
+        if anyException:
+            raise anyException
+    except CouchbaseError as e:
+        if (e is KeyExistsError):
+            logger.debug('insertLine: Exception={3}, FileName={0}, LineNo={1}, Line={2}'.format(
+                filename, lineno, line, "RowDuplicateException"))
+            raise RowDuplicateException(filename, lineno, line)
+        else:
+            logger.debug('insertLine: Exception={3}, FileName={0}, LineNo={1}, Line={2}'.format(
+                filename, lineno, line, e.__class__))
+            raise RowException(filename, lineno, line)
+
+
 def updateLine(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line):
-	linevalues = lineToDictionary(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
-	linelist = startProcess.cb.get(linevalues['dictline']).value
-	startProcess.cb.set(linevalues['linekey'], linelist)
-	
+    linevalues = lineToDictionary(
+        filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
+    linelist = startProcess.cb.get(linevalues['dictline']).value
+    startProcess.cb.set(linevalues['linekey'], linelist)
+
+
 def deleteLine(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line):
-	linevalues = lineToDictionary(filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
-	
-	try:
-		startProcess.cb.delete(linevalues['linekey'])
-	except CouchbaseError as e:
-		print ( "deleteLine: Exception: " + str(e.key))	
-		raise
+    linevalues = lineToDictionary(
+        filename, tenantid, documentType, feedtype, rowkeyschema, rowschema, lineno, line)
+
+    try:
+        startProcess.cb.delete(linevalues['linekey'])
+    except CouchbaseError as e:
+        print("deleteLine: Exception: " + str(e.key))
+        raise
+
 
 @app.task(base=DatabaseTask)
 def startErrorReportProcess(tenantid, documentType):
-	path = "feeds/"
-	DocumentType = documentType
-
-	for result in View(startErrorReportProcess.cb, "dev_Tenant1CustomerRejected", "Tenant1CustomerRejected", include_docs=True):
-		logger.info("emitted key: {0}, doc_id: {1}".format(result.key, result.doc.value))
+    path = "feeds/"
+    DocumentType = documentType
+    viewName = "Tenant|{0}|{1}|{2}".format(tenantid, "CUSTOMER", "REJECTED")
+    viewDesignName = "{0}_{1}".format("dev", viewName)
+    for result in View(startErrorReportProcess.cb, viewDesignName, viewName, include_docs=True):
+        logger.info(
+            "emitted key: {0}, doc_id: {1}".format(result.key, result.doc.value))
--- flask/FeedConfigManager/static/data/cb_views.js
+++ flask/FeedConfigManager/static/data/cb_views.js
@@ -1,12 +1,12 @@
 
-// NAME: Tenant1Customer
+// NAME: Tenant|1|CUSTOMER
 // DESCRIPTION: This view shows all documents of CUSTOMER for tenant 1
 function (doc, meta) {
   if ( doc.tenantid == 1 && doc.feedtype == 'CUSTOMER')
   	emit(meta.id, doc);
 }
 
-// NAME: Tenant1CustomerRejected
+// NAME: Tenant|1|CUSTOMER|REJECTED
 // DESCRIPTION: This view shows all documents of CUSTOMER for tenant 1 that are rejected
 function (doc, meta) {
   if ( doc.tenantid == 1 && doc.feedtype == 'CUSTOMER' && (doc.errorflag == 1 || doc.rejectedflag == 1))
